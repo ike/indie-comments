44b901259b7e94ac301fea959a374a19137bb264
Isaac Lewis <ikelewis678@gmail.com>
2015-10-29 23:41:01 -0700

    remove json parsers from server

--- server/server.js
+++ server/server.js
@@ -17,11 +17,6 @@ app.use(function(req, res, next) {
 app.use(enchilada(path.join(root, 'client')));
 app.use('/', express.static(path.join(root, 'client'), {index: 'index.html'}));
 
-// JSON and Form parsers
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-
 // Start the server
 app.listen(3000);
 console.log('Server listening on http://localhost:3000');
